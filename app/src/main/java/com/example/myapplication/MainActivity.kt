d2e4aadfd2fd9c0cecd905a421b323b327c2cfe4
aleksandre-tsulukidze <aleko.wulukidze@gmail.com>
2021-05-15 16:28:42 +0400

    calculator start

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultTextView: TextView
+    private var operand: Double = 0.0
+    private var operation: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.resultTextView = findViewById(R.id.resultTextView)
+    }
+
+    fun numberClick(clickedView: View) {
+        if (clickedView is Button) {
+            val number: String = clickedView.text.toString()
+            var result: String = this.resultTextView.text.toString()
+
+            if (result == "0") {
+                result = ""
+            }
+
+            resultTextView.text = "$result$number"
+        }
+    }
+
+    fun operationClick(view: View) {
+        if (view is Button) {
+
+            if (resultTextView.text.isNotEmpty()) {
+                operand = resultTextView.text.toString().toDouble()
+            }
+
+            this.operation = view.text.toString()
+            this.resultTextView.text = ""
+
+        }
+    }
+
+    fun equalsClick(view: View) {
+        val secOperandText: String = this.resultTextView.text.toString()
+        var secOperand = 0.0
+
+        if (secOperandText.isNotEmpty()) {
+            secOperand = secOperandText.toDouble()
+        }
+
+        when (this.operation) {
+            "+" -> this.resultTextView.text = (this.operand + secOperand).toString()
+            "-" -> this.resultTextView.text = (this.operand - secOperand).toString()
+            "*" -> this.resultTextView.text = (this.operand * secOperand).toString()
+            "/" -> this.resultTextView.text = (this.operand / secOperand).toString()
+        }
     }
 }
